2446b7014f45e18e24063dea9f61c872b6cc0131
yuriy-arttime <yuriy3304@gmail.com>
2021-04-20 19:22:53 +0300

    add api script for read birdlife data and add legend switch

--- api/controllers/birdlife.js
+++ api/controllers/birdlife.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 function getBirdlifeShape(req, res) {
-  const filePathShape = `public/json/birdlife/index.json`;
+  const filePathShape = 'public/json/birdlife/index.json';
   try {
     const data = fs.readFileSync(filePathShape);
-    res.json(JSON.parse(data));  
+    res.json(JSON.parse(data));
   } catch (err) {
     res.status(err.statusCode || 500);
     res.json({ error: err.message });
